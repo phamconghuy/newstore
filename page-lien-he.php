780bfcb974fca7ea6945ee05134cbd2789dd6044
Dao <sydaonguyen1999@gmail.com>
2019-11-30 12:12:50 +0700

    ffff

--- page-lien-he.php
+++ page-lien-he.php
@@ -15,7 +15,7 @@ do_action( 'newstore_before_blog_post');
                         </div>
                         <!-- contact map -->
                         <div class="aa-contact-map">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.1307306485687!2d106.67560421462154!3d10.877660060291829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d7d6d75b7305%3A0x9cc6442d4b22af76!2zSMOgIEh1eSBHacOhcCwgVGjhuqFuaCBM4buZYywgUXXhuq1uIDEyLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1575089834548!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.1307306485687!2d106.67560421462154!3d10.877660060291829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d7d6d75b7305%3A0x9cc6442d4b22af76!2zSMOgIEh1eSBHacOhcCwgVGjhuqFuaCBM4buZYywgUXXhuq1uIDEyLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1575089834548!5m2!1svi!2s" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
                         </div>
                         <!-- Contact address -->
                         <div class="aa-contact-address">
